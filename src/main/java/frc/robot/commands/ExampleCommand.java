7578dd2fa08ca31f9efca6b977a0bc094d3b72fd
dakott <dakott@pobox.com>
2020-01-28 14:29:54 -0500

    Updated ExampleCommand and ExampleSubsystem to match 2020's autogenerated templates to make it easier for new modules to emulate these example commands and subsystems.

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -36,17 +36,15 @@ public class ExampleCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    System.out.println("heheh");
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println(interrupted);
   }
 
   // Returns true when the command should end.
-  @Override 
+  @Override
   public boolean isFinished() {
     return false;
   }
