1b4ed554776a0c36576bcba569877bb890536a92
dakott <dakott@pobox.com>
2020-02-11 13:51:14 -0500

    Fixed Indexer constants so the project compiles *looks sternly, but benignly at JS and LO*

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,71 +21,72 @@ import frc.robot.Gains;
  * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-	public double controlPanelWheelEncoderCountsToRPM(int encoderCounts) {
+	public static double controlPanelWheelEncoderCountsToRPM(int encoderCounts) {
 		double RPM;
 		RPM = encoderCounts / 1.0; // TODO: determine current constant to use here
 		return RPM;
 	}
 
-	public int controlPanelWheelAngleToEncoderCounts(int degrees) {
+	public static int controlPanelWheelAngleToEncoderCounts(int degrees) {
 		int encoderCounts;
 		encoderCounts = degrees * 200; // TODO: find out the number because we dont know
 		return encoderCounts;
 	}
 
-	public static int driverController = 0;
-	public static int operatorController = 1;
+	public static final int driverController = 0;
+	public static final int operatorController = 1;
 
-	public static int controlPanelMotorControllerCANId = 4;
-	public static int rotationControlButton = 5;
-	public static int positionControlButton = 6;
-	public static I2C.Port i2cPortColorSensor = I2C.Port.kOnboard; // use for color sensor
-	public static double controlPanelSpeedMax = 60.0; // RPM
-	public static double controlPanelMaxAcceleration = 60.0; // RPM/s
-	public static double controlPanelStopMotor = 0.0;
-	public static int controlPanelEncoderCounts = 4000; // hypothetical value between 3-5 rotations --more than 3//
-	public static int controlPanelVibrationTime = 1000; // miliseconds
-	public static int acceptableErrorControlPanel = 100; // acceptable error for control panel spinning
+	public static final int controlPanelMotorControllerCANId = 4;
+	public static final int rotationControlButton = 5;
+	public static final int positionControlButton = 6;
+	public static final I2C.Port i2cPortColorSensor = I2C.Port.kOnboard; // use for color sensor
+	public static final double controlPanelSpeedMax = 60.0; // RPM
+	public static final double controlPanelMaxAcceleration = 60.0; // RPM/s
+	public static final double controlPanelStopMotor = 0.0;
+	public static final int controlPanelEncoderCounts = 4000; // hypothetical value between 3-5 rotations --more than
+																// 3//
+	public static final int controlPanelVibrationTime = 1000; // miliseconds
+	public static final int acceptableErrorControlPanel = 100; // acceptable error for control panel spinning
 
-	public static int intakeMotorControllerCANId = 5;
-	public static int indexerMotorControllerCANId = 6;
-	public static int kickerMotorControllerCANId = 7;
-	public static int shooterMotorControllerCANId = 8;
-	public static double intakeMotorSpeed = 60.0; // RPM
-	public static double indexerMotorSpeed = 60.0; // RPM
-	public static double kickerMotorSpeed = -1.0; // percent
-	public static double kickerCaptureSpeed = 0.61; // percent
-	public static double shooterIntendedSpeed = 2500.0; // RPM
-	public static double shooterIntendedSpeedTolerance = 0.05;
-	public static double shooterYeetSpeedInitiation = 4600.0; //RPM
-	public static double shooterYeetSpeedWall = 4000.0; //RPM
+	public static final int intakeMotorControllerCANId = 5;
+	public static final int indexerMotorControllerCANId = 6;
+	public static final int kickerMotorControllerCANId = 7;
+	public static final int shooterMotorControllerCANId = 8;
+	public static final double intakeMotorSpeed = 60.0; // RPM
+	public static final double indexerMotorSpeed = 60.0; // RPM
+	public static final double kickerMotorSpeed = -1.0; // percent
+	public static final double kickerCaptureSpeed = 0.61; // percent
+	public static final double shooterIntendedSpeed = 2500.0; // RPM
+	public static final double shooterIntendedSpeedTolerance = 0.05;
+	public static final double shooterYeetSpeedInitiation = 4600.0; // RPM
+	public static final double shooterYeetSpeedWall = 4000.0; // RPM
 	// TODO: find actual speeds of motors
-	public static int visionCameraPort = 0;
-	public static int frontCamera = 1;
-	public static int rearCamera = 2;
+	public static final int visionCameraPort = 0;
+	public static final int frontCamera = 1;
+	public static final int rearCamera = 2;
 
-	//elevator
-	public static int maxWinchEncoderCounts =  10000 ; //get winch value
-	public static int minWinchEncoderCounts = 0 ;
-	//public static int winchEncoderchannel  = 0;
-	public static int raiseElevatorButton = 1; //get buttons value from SAD
-	public static int lowerElevatorButton = 1;
-	public static int engageUntitledGooseButton = 1;
-	public static int disengageUntitledGooseButton = 1;
-	public static double latchReleaseEncoderCount = 500.0; //(automatic???)
-	public static double goosehookMotorSpeed = 0.50;
+	// elevator
+	public static final int maxWinchEncoderCounts = 10000; // get winch value
+	public static final int minWinchEncoderCounts = 0;
+	// public static int winchEncoderchannel = 0;
+	public static final int raiseElevatorButton = 1; // get buttons value from SAD
+	public static final int lowerElevatorButton = 1;
+	public static final int engageUntitledGooseButton = 1;
+	public static final int disengageUntitledGooseButton = 1;
+	public static final double latchReleaseEncoderCount = 500.0; // (automatic???)
+	public static final double goosehookMotorSpeed = 0.50;
 
-	public static int raiseElevatorButtonjoystick = 1; // 1 for operator, 0 for driver, tbd
-	public static int lowerElevatorButtonjoystick = 1;
-	public static int engageUntitledGooseButtonjoystick = 1;
-	public static int disengageUntitledGooseButtonjoystick = 1;
-	public static boolean kElevatorSensorPhase = false;
+	public static final int raiseElevatorButtonjoystick = 1; // 1 for operator, 0 for driver, tbd
+	public static final int lowerElevatorButtonjoystick = 1;
+	public static final int engageUntitledGooseButtonjoystick = 1;
+	public static final int disengageUntitledGooseButtonjoystick = 1;
+	public static final boolean kElevatorSensorPhase = false;
 
-	public static int winchMotorElevatorCANId = 7; // change back to 7 for final bot
-	public static int untitledGooseMotorCANId = 9; //change back to 9
+	public static final int winchMotorElevatorCANId = 7; // change back to 7 for final bot
+	public static final int untitledGooseMotorCANId = 9; // change back to 9
 
-	public static double driveMotorRampRate = 0.5; //number = seconds till full speed
-	public static double rotationInQuarter = 0.4;
+	public static final double driveMotorRampRate = 0.5; // number = seconds till full speed
+	public static final double rotationInQuarter = 0.4;
 
 	/**
 	 * Which PID slot to pull gains from. Starting 2018, you can choose from 0,1,2
@@ -107,28 +108,58 @@ public final class Constants {
 	public static final int kTimeoutMs = 30;
 
 	/* Choose so that Talon does not report sensor out of phase */
-	public static boolean kSensorPhase = true;
+	public static final boolean kSensorPhase = true;
 
 	/**
 	 * Choose based on what direction you want to be positive, this does not affect
 	 * motor invert.
 	 */
-	public static boolean kMotorInvert = false;
+	public static final boolean kMotorInvert = false;
 
 	/**
 	 * Gains used in Position Closed Loop, to be adjusted accordingly Gains(kp, ki,
 	 * kd, kf, izone, peak output);
 	 */
 	public static final Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
-	public static final String kGains_Indexer = null;
 
-	public static int frontLeftMotor = 13;
-	public static int rearLeftMotor = 12;
-	public static int frontRightMotor = 2;
-	public static int rearRightMotor = 3;
-	public static double closedLoopRampRate = 0.1; // Volts / ms
+	/*
+	 * TalonSRX's PIDF gains are calculated base on 1023 as the maximum output,
+	 * which is 100% duty cycle, which is all of the Talon's speed and capability.
+	 * Thus, if you want to drive the TalonSRX at 100% if the error is 200 units
+	 * (e.g. RPM for Velocity mode, or encoder counts for position mode), divide
+	 * 1023 by 200 to get your Kp gain:
+	 * 
+	 * Kp = 1023.0 / ErrorAt100%Output
+	 * 
+	 * Kp = 1023.0 / 200.0
+	 * 
+	 * Kp = 5.12
+	 * 
+	 * The TalonSRX's measure velocity in units of encoder counts per interval of
+	 * 0.1 seconds. So, if you're spinning at 100 RPM, and your encoder has 2048
+	 * counts per revolution, that's equal to:
+	 * 
+	 * CountsPerInterval(100 RPM) = 100 revolution/minute * 2048 counts/revolution *
+	 * minute/60 second * second/10 interval
+	 * 
+	 * CountsPerInterval(100 RPM) = (100 * 2048) / (60 * 10)
+	 * 
+	 * CountsPerInterval(100 RPM) = 341.33 counts per interval
+	 * 
+	 * Put this all together to compute the Kp for a TalonSRX in Closed Loop
+	 * velocity mode with an encoder that has 2048 counts per revolution:
+	 * 
+	 * Kp = 1023.0 / 341.33
+	 * 
+	 * Kp = 2.997
+	 */
 
-	// public static int neoEncoderLeft = 5;
-	// public static int neoEncoderRight = 6;
+	public static final Gains kGains_Indexer = new Gains(2.997, 0.0, 0.0, 0.0, 0, 1.0);
+
+	public static final int frontLeftMotor = 13;
+	public static final int rearLeftMotor = 12;
+	public static final int frontRightMotor = 2;
+	public static final int rearRightMotor = 3;
+	public static final double closedLoopRampRate = 0.1; // Volts / ms
 
 }
