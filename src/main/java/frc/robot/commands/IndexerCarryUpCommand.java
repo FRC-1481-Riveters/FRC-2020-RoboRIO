34d94a95499535505c4c562a8881edcab032a815
dakott@pobox.com <dakott@pobox.com>
2020-02-10 20:42:35 -0500

    worked on step 8 of indexer

--- src/main/java/frc/robot/commands/IndexerCarryUpCommand.java
+++ src/main/java/frc/robot/commands/IndexerCarryUpCommand.java
@@ -8,18 +8,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Indexer;
 
 public class IndexerCarryUpCommand extends CommandBase {
+  private Indexer m_indexer;
   /**
    * Creates a new indexerCarryUpCommand.
    */
-  public IndexerCarryUpCommand() {
+  public IndexerCarryUpCommand(Indexer subsystem) {
+    m_indexer = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_indexer.setClosedLoopSpeed(Constants.indexerMotorSpeed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -30,6 +36,7 @@ public class IndexerCarryUpCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_indexer.setClosedLoopSpeed(0.0);
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -7,18 +7,46 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Indexer extends SubsystemBase {
+  private static WPI_TalonSRX m_indexer = new WPI_TalonSRX(Constants.indexerMotorControllerCANId);
+
   /**
    * Creates a new Indexer.
    */
+ 
+  public void setClosedLoopSpeed(double RPM) {
+    double targetVelocity_UnitsPer100ms = RPM  * 4096 / 600;
+    m_indexer.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
+  }
+
   public Indexer() {
+
+    m_indexer.configNominalOutputForward(0, Constants.kTimeoutMs);
+    m_indexer.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    m_indexer.configPeakOutputForward(1, Constants.kTimeoutMs);
+    m_indexer.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+    /* Config the Velocity closed loop gains in slot0 */
+		m_indexer.config_kF(Constants.kPIDLoopIdx, Constants.kGains_Indexer.kF, Constants.kTimeoutMs);
+		m_indexer.config_kP(Constants.kPIDLoopIdx, Constants.kGains_Indexer.kP, Constants.kTimeoutMs);
+	  m_indexer.config_kI(Constants.kPIDLoopIdx, Constants.kGains_Indexer.kI, Constants.kTimeoutMs);
+		m_indexer.config_kD(Constants.kPIDLoopIdx, Constants.kGains_Indexer.kD, Constants.kTimeoutMs);
+
+    // m_indexer.setInverted(Constants.add constant here);
+    setClosedLoopSpeed(0.0);
 
   }
 
   @Override
   public void periodic() {
+
     // This method will be called once per scheduler run
+
   }
 }
