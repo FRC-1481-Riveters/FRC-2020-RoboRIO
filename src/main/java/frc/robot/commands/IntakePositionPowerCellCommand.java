e081c2a9113a8171860f1a001e4015b730f6f8e9
dakott <dakott@pobox.com>
2020-02-20 14:19:55 -0500

    Stripped out PID stuff from IntakePositionPowerCellCommand in preparation for simpler design. "The design is dead. Long live the design!"

--- src/main/java/frc/robot/commands/IntakePositionPowerCellCommand.java
+++ src/main/java/frc/robot/commands/IntakePositionPowerCellCommand.java
@@ -7,26 +7,16 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.Intake;
 
-public class IntakePositionPowerCellCommand extends PIDCommand {
+public class IntakePositionPowerCellCommand extends CommandBase {
   /**
    * Creates a new IntakePositionPowerCellCommand.
    */
-  public IntakePositionPowerCellCommand(double howCloseIsThePowerCell, Intake m_intake) {
-    super(new PIDController(Constants.kGains.kP, Constants.kGains.kI, Constants.kGains.kD),
-    m_intake.getDistanceToPowerCellMeasurer(),
-    howCloseIsThePowerCell,
-    output -> m_intake.setSpeed(output),
-    m_intake
-    );
+  public IntakePositionPowerCellCommand(Intake m_intake) {
     // Use addRequirements() here to declare subsystem dependencies.
-    getController()
-      .setTolerance(Constants.howCloseIsThePowerCellTolerance);
   }
 
   // Called when the command is initially scheduled.
@@ -47,6 +37,6 @@ public class IntakePositionPowerCellCommand extends PIDCommand {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return getController().atSetpoint();
+    return false;
   }
 }
