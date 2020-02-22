a08851958cb4b5ff69165018a2da1124ff10b936
klhansen4 <klhansen4@gmail.com>
2020-02-22 18:30:03 -0500

    Improved Shooter PID coefficients to reduce overshoot (and make more consisten initial shots) and reduce shooting speed variation

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,12 +7,17 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.revrobotics.CANEncoder;
+import com.revrobotics.CANError;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
@@ -23,8 +28,14 @@ public class Shooter extends SubsystemBase implements Sendable {
   private CANPIDController m_pidController;
   private CANEncoder m_encoder;
   private double m_shooterIntendedSpeed = 10000.0;
-  protected YeetDetector m_yeetDetector= new YeetDetector(this::isAtSpeed);
-    /**
+  protected YeetDetector m_yeetDetector = new YeetDetector(this::isAtSpeed);
+
+  private NetworkTableEntry shooterKp;
+  private NetworkTableEntry shooterKi;
+  private NetworkTableEntry shooterKd;
+  private NetworkTableEntry shooterKf;
+
+  /**
    * Creates a new sparkMaxTest.
    */
   public Shooter() {
@@ -57,13 +68,47 @@ public class Shooter extends SubsystemBase implements Sendable {
     m_encoder = m_motor.getEncoder();
 
     // set PID coefficients
-    m_pidController.setP(2.2e-4);
-    m_pidController.setI(.00000005);
+    m_pidController.setP(0.00004);
+    m_pidController.setI(.000000005);
     m_pidController.setD(0.00001);
-    m_pidController.setFF(1.5e-4);
+    m_pidController.setFF(0.00018);
 
     setClosedLoopSpeed(0.0);
 
+    shooterKp = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Shooter kP");
+    shooterKi = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Shooter kI");
+    shooterKd = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Shooter kD");
+    shooterKf = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("Shooter kF");
+
+    shooterKp.setDouble(m_pidController.getP());
+    shooterKi.setDouble(m_pidController.getI());
+    shooterKd.setDouble(m_pidController.getD());
+    shooterKf.setDouble(m_pidController.getFF());
+
+    /*
+     * This is the maximum velocity of the indexer in units of encoder counts per
+     * 100 ms (a decisecond)
+     */
+
+    shooterKp.addListener(event -> {
+      while (m_pidController.setP(SmartDashboard.getNumber("Shooter kP", 0.0), 0) != CANError.kOk)
+        ;
+    }, EntryListenerFlags.kUpdate);
+
+    shooterKi.addListener(event -> {
+      while (m_pidController.setI(SmartDashboard.getNumber("Shooter kI", 0.0), 0) != CANError.kOk)
+        ;
+    }, EntryListenerFlags.kUpdate);
+
+    shooterKd.addListener(event -> {
+      while (m_pidController.setD(SmartDashboard.getNumber("Shooter kD", 0.0), 0) != CANError.kOk)
+        ;
+    }, EntryListenerFlags.kUpdate);
+
+    shooterKf.addListener(event -> {
+      while (m_pidController.setFF(SmartDashboard.getNumber("Shooter kF", 0.0), 0) != CANError.kOk)
+        ;
+    }, EntryListenerFlags.kUpdate);
 
   }
 
@@ -87,7 +132,7 @@ public class Shooter extends SubsystemBase implements Sendable {
     m_shooterIntendedSpeed = RPM;
 
     if (m_shooterIntendedSpeed > 10.0) {
-    m_pidController.setReference(RPM, ControlType.kVelocity);
+      m_pidController.setReference(RPM, ControlType.kVelocity);
     } else {
       m_motor.set(0.0);
     }
@@ -117,19 +162,14 @@ public class Shooter extends SubsystemBase implements Sendable {
 
   public void initSendable(SendableBuilder builder) {
     super.initSendable(builder);
-    builder.setActuator(true);
 
     builder.addDoubleProperty("ClosedLoopSpeed(RPM)", this::getSpeed, this::setClosedLoopSpeed);
     builder.addDoubleProperty("CommandedSpeed(RPM)", () -> {
       return m_shooterIntendedSpeed;
     }, null);
-    builder.addDoubleProperty("OpenLoopSpeed(normalized)", m_motor::get, m_motor::set);
-    builder.addDoubleProperty("kP", m_pidController::getP, m_pidController::setP);
-    builder.addDoubleProperty("kI", m_pidController::getI, m_pidController::setI);
-    builder.addDoubleProperty("kD", m_pidController::getD, m_pidController::setD);
-    builder.addDoubleProperty("kF", m_pidController::getFF, m_pidController::setFF);
+    builder.addDoubleProperty("OpenLoopSpeed(normalized)", m_motor::get, null);
     builder.addDoubleProperty("MotorOutputDrive", m_motor::getAppliedOutput, null);
-    builder.addDoubleProperty("Detected Yeets",this::getTotalYeets,null);
+    builder.addDoubleProperty("Detected Yeets", this::getTotalYeets, null);
   }
 
 }
